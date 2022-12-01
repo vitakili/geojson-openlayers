b6cce99fbcbd2369c8a8a1bd022e8a80f1461f2d
Vít Kilián <vitula2000@seznam.cz>
2022-12-01 15:13:32 +0100

    loading state and added proxy to client folder app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,48 @@
-import './App.css';
+import "./App.css";
 
 // react
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 // openlayers
-import GeoJSON from 'ol/format/GeoJSON'
-import Feature from 'ol/Feature';
+import GeoJSON from "ol/format/GeoJSON";
+import Feature from "ol/Feature";
 
 // components
-import MapView from './components/MapView';
-import { Geometry } from 'ol/geom';
+import MapView from "./components/MapView";
+import { Geometry } from "ol/geom";
 
 function App() {
-  
   // set intial state
-  const [ features, setFeatures ] = useState<Feature<Geometry>[]>()
+  const [features, setFeatures] = useState<Feature<Geometry>[]>();
 
-  // initialization - retrieve GeoJSON features from Mock JSON API get features from mock 
+  // initialization - retrieve GeoJSON features from Mock JSON API get features from mock
   //  GeoJson API (read from flat .json file in public directory)
-  useEffect( () => {
-
-    fetch('http://localhost:5000/api/mhd')
-      .then(response => response.json())
-      .then( (fetchedFeatures) => {
-
+  useEffect(() => {
+    fetch("http://localhost:5000/api/mhd")
+      .then((response) => response.json())
+      .then((fetchedFeatures) => {
         // parse fetched geojson into OpenLayers features
         //  use options to convert feature from EPSG:4326 to EPSG:3857
         const wktOptions = {
-          dataProjection: 'EPSG:4326',
-          featureProjection: 'EPSG:3857'
-        }
-        const parsedFeatures = new GeoJSON().readFeatures(fetchedFeatures, wktOptions)
+          dataProjection: "EPSG:4326",
+          featureProjection: "EPSG:3857",
+        };
+        const parsedFeatures = new GeoJSON().readFeatures(
+          fetchedFeatures,
+          wktOptions
+        );
 
         // set features into state (which will be passed into OpenLayers
         //  map component as props)
-        setFeatures(parsedFeatures)
-      })
-      
-    },[])
-    
+        setFeatures(parsedFeatures);
+      });
+  }, []);
+
   return (
-    <div className="App">  
-      <MapView features={features} zoom={12} />
+    <div className="App">
+      {!features ? "Loading..." : <MapView features={features} zoom={12} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 // const mhdData = require('./assets/mhdzastavky.geojson');
 var fs = require('fs');
 
 const app = express();
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get('/api/mhd', cors(), function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   fs.createReadStream('./assets/mhdzastavky.geojson').pipe(res);
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 const port = 5000;
